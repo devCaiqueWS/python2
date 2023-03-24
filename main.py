bc27c18cb74d3fb53feba612b341505d2b6e892c
Caique <dev_caiquews@outlook.com>
2023-03-24 20:06:10 -0300

    save1

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+
+i=0
+while i<=100:
+    print(i)
+    i+=5
+    
+
+'''
+i=0
+
+
+while i<5:
+    num1 = int(input("Digite um numero: "))
+
+    if num1%2==0:
+        print(f"{num1} é par")
+    else:
+        print(f"{num1} é impar")
+
+    i+=1
+
